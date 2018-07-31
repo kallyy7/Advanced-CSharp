b57317e8681f15a5326aa6d083a3adad242796ed
Kalina Milanova <milanova1203@gmail.com>
2018-07-31 21:05:16 +0300

    Add files via upload

---
+++ 03.Functional Programming/Functional Programming - Lab/Functional Programming - Exercises/01.ActionPrint/ActionPrint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public class ActionPrint
+{
+    public static void Main()
+    {
+        var inputNames = Console.ReadLine()
+            .Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+            .ToList();
+        inputNames
+            .ForEach(x => Console.WriteLine(x));
+    }
+}
---
+++ 03.Functional Programming/Functional Programming - Lab/Functional Programming - Exercises/03.CustomMinFunction/CustomMinFunction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+public class CustomMinFunction
+{
+    public static void Main()
+    {
+        var inputNumbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+        Func<int[], int> minFunc = GetMinNumber;
+        
+        Console.WriteLine(minFunc(inputNumbers));
+    }
+
+    private static int GetMinNumber(int[] numbers)
+    {
+        return numbers.Min();
+    }
+}
