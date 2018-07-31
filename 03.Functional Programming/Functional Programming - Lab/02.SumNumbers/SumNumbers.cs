8086b3fe0f5977922292d9a3adfcfc1fe3bd5473
Kalina Milanova <milanova1203@gmail.com>
2018-07-31 19:57:23 +0300

    Add files via upload

---
+++ 03.Functional Programming/Functional Programming - Lab/02.SumNumbers/SumNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public class SumNumbers
+{
+    public static void Main()
+    {
+        var input = Console.ReadLine()
+            .Split(" ,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        Console.WriteLine(input.Length);
+        Console.WriteLine(input.Sum());
+    }
+}
---
+++ 03.Functional Programming/Functional Programming - Lab/05.FilterByAge/FilterByAge.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class FilterByAge
+{
+    public static void Main()
+    {
+        int numberOfPeople = int.Parse(Console.ReadLine());
+        var dataPeople = new Dictionary<string, int>();
+
+        for (int i = 0; i < numberOfPeople; i++)
+        {
+            string[] inputInfo = Console.ReadLine()
+                .Split(" ,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            string currentName = inputInfo[0];
+            int currentAge = int.Parse(inputInfo[1]);
+
+            dataPeople.Add(currentName, currentAge);
+        }
+
+        string inputCondition = Console.ReadLine();
+        int inputAge = int.Parse(Console.ReadLine());
+        string inputFormat = Console.ReadLine();
+
+        dataPeople = Condition(inputCondition, inputAge, dataPeople);
+        FormatPrint(inputFormat, dataPeople);
+    }
+
+    private static Dictionary<string, int> Condition(string condition, int age, Dictionary<string, int> people)
+    {
+        if (condition.Equals("older"))
+        {
+            people = people.Where(x => x.Value >= age).ToDictionary(x => x.Key, x => x.Value);
+        }
+        else
+        {
+            people = people.Where(x => x.Value < age).ToDictionary(x => x.Key, x => x.Value);
+        }
+
+        return people;
+    }
+
+    private static void FormatPrint(string format, Dictionary<string, int> people)
+    {
+        if (format.Equals("name age"))
+        {
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{person.Key} - {person.Value}");
+            }
+        }
+        else if (format.Equals("name"))
+        {
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{person.Key}");
+            }         
+        }
+        else if (format.Equals("age"))
+        {
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{person.Value}");
+            }
+        }
+
+        return;
+    }
+}
