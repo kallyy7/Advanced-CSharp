1856c74b0f5a496cb5b401f86da803790bfafacb
kallyy7 <milanova1203@gmail.com>
2018-08-18 01:43:15 +0300

    .

---
+++ 03.Functional Programming - Exercises/02.KnightsOfHonor/KnightsOfHonor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public class KnightsOfHonor
+{
+    public static void Main()
+    {
+        var inputNames = Console.ReadLine()
+            .Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+            .ToList();
+        inputNames
+            .ForEach(x => Console.WriteLine($"Sir {x}"));
+    }
+}
---
+++ 03.Functional Programming - Lab/01.SortEvenNumbers/SortEvenNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public class SortEvenNumbers
+{
+    public static void Main()
+    {
+        var input = Console.ReadLine()
+            .Split(" ,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .Where(x => x % 2 == 0)
+            .OrderBy(x => x);
+
+        Console.WriteLine(string.Join(", ", input));
+    }
+}
